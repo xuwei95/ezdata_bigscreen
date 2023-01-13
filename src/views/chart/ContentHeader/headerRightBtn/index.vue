85b5aefb0ff2bce2c38339b1b88f9654b4471388
奔跑的面条 <1262327911@qq.com>
2023-01-13 16:52:10 +0800

    build: 升级版本到 1.1.9

--- src/views/chart/ContentHeader/headerRightBtn/index.vue
+++ src/views/chart/ContentHeader/headerRightBtn/index.vue
@@ -56,7 +56,7 @@ const previewHandle = () => {
 // 发布
 const sendHandle = () => {
   goDialog({
-    message: '想体验发布功能，请前往 master-fetch 分支查看: https://gitee.com/MTrun/go-view/tree/master-fetch',
+    message: '想体验发布功能，请前往 master-fetch 分支查看: https://demo.mtruning.club/#/login',
     positiveText: '了然',
     closeNegativeText: true,
     onPositiveCallback: () => {}
