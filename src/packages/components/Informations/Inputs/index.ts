1fd73cbc57a7c120daffc1f31b433cd53271a362
luoyp <2456821174@qq.com>
2023-08-01 09:32:55 +0800

    新增分页联动组件

--- src/packages/components/Informations/Inputs/index.ts
+++ src/packages/components/Informations/Inputs/index.ts
@@ -1,5 +1,6 @@
 import { InputsDateConfig } from './InputsDate/index'
 import { InputsSelectConfig } from './InputsSelect/index'
 import { InputsTabConfig } from './InputsTab/index'
+import { InputsPaginationConfig } from "./InputsPagination/index";
 
-export default [InputsDateConfig, InputsSelectConfig, InputsTabConfig]
+export default [InputsDateConfig, InputsSelectConfig, InputsTabConfig,InputsPaginationConfig]
